7faaec30168c98e298109a77c61c5b27e76306c4
松浦 <2210344@ecc.ac.jp>
2024-06-11 16:02:39 +0900

    first commit

---
+++ SeminarChatSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeminarChatSwift
+//
+//  Created by 松浦壮吾 on 2024/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeminarChatSwift/SeminarChatSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeminarChatSwiftApp.swift
+//  SeminarChatSwift
+//
+//  Created by 松浦壮吾 on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct SeminarChatSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
